c8e33715e7cfa6676dd15ba98f65b165c84ac602
niranjan-mj <niranjankumar.mj@vvdntech.in>
2019-04-17 16:13:57 +0530

    todo app

--- todolist/models.py
+++ todolist/models.py
@@ -13,23 +13,10 @@ class TodoList(models.Model):
     title = models.CharField(max_length=100)
     # content = models.TextField(max_length=200)
     created = models.DateTimeField(auto_now_add=True)
-    due_date = models.DateTimeField(default=datetime.datetime.now())
+    due_date = models.DateTimeField()
 
     class Meta:
         ordering = ["-created"]
 
     def __str__(self):
         return self.title
-
-    # def check_date(self, due_date, created):
-    #     if due_date <= created:
-    #         print("datecheck")
-    #         raise forms.ValidationError("please enter a valid due_date")
-    #     return due_date
-    #
-    # def save(self, *args, **kwargs):
-    #     if not self.id:
-    #         self.due_date = self.check_date(self.due_date, self.created)
-    #     super(TodoList, self).save(*args, **kwargs)
-    #
-    #
--- todolist/views.py
+++ todolist/views.py
@@ -1,49 +1,37 @@
 import datetime
-
 from django.shortcuts import render, redirect
-
 from todolist.models import TodoList
-
-
 
 def index(request):
     todo = TodoList.objects.all()
 
     if request.method == "POST":
-        print(request.POST, "hi")
-
         if "taskAdd" in request.POST:
             author = request.POST['author']
             if author == '':
                 author = 'anonymous'
-
             title = request.POST["title"]
             description = request.POST["description"]
             created = str(datetime.date.today())
-
             due_date = str(request.POST["duedate"])
             datetime_object = datetime.datetime.strptime(due_date, "%Y-%m-%d").date()
-            print(datetime_object, 'checkdatetime')
-            if due_date <= created:
-                print(datetime.date.today()+datetime.timedelta(days=1))
-                due_date = str(datetime.date.today()+datetime.timedelta(days=1))
-            else:
-                due_date = due_date
-
-
+            if datetime_object <= datetime.date.today():
+                due_date = str(datetime.date.today() + datetime.timedelta(days=1))
+            due_date = due_date
             todo = TodoList(author = author, title = title, created = created,due_date = due_date, description = description)
             todo.save()
             return redirect("/todo/")
 
-    #     print("testing")
-    #     if "delete" in request.POST:
-    #         print("testing")
-    #         print("testing1",request.POST.get('delete'))
-    #         checkedlist =  request.POST.get('delete')
-    #         todo = TodoList.objects.get(id=int(checkedlist))
-    #         todo.delete()
-    #
+        if "delete" in request.POST:
+            checkedlist = request.POST["delete"]
+            try:
+                todo = TodoList.objects.filter(id=checkedlist)
+                todo.delete()
+            except todo.DoesNotExist:
+                return "id does not exist"
+
     return render(request, "index.html", {"todos": todo})
+
 
 
 
