afdcdead2ad8b1e855e75540e88752babe70b4a0
niranjan-mj <niranjankumar.mj@vvdntech.in>
2019-04-24 15:10:57 +0530

    new

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -123,4 +123,5 @@ STATIC_URL = '/static/'
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+django_heroku.settings(locals())
 # STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
